0feeb8583f21a2838d9ab77b4c64352a3df43d12
harryphma <phamquanghung2705@gmail.com>
2025-01-05 20:11:27 -0500

    update submit button

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,5 +1,6 @@
 import { useActionState, use } from "react";
 import { OpinionsContext } from "../store/opinions-context";
+import Submit from "./Submit.jsx";
 
 
 export function NewOpinion() {
@@ -63,9 +64,7 @@ export function NewOpinion() {
           {formState.errors.map((error) => (<li key={error}>{error}</li>))}
           </ul>}
 
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
+        <Submit></Submit>
       </form>
     </div>
   );
